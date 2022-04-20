29bf6e744f2a2b214f7cb119feff6bcde832d759
ashvarvar <102923516+ashvarvar@users.noreply.github.com>
2022-04-21 01:04:15 +0300

    Add files via upload

---
+++ Home_Work_3/HW2_zadacha_19/Program.cs
@@ -0,0 +1,27 @@
+﻿//принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+
+
+Console.WriteLine("Введите пятизначное число N: ");
+string number = Console.ReadLine();
+int length = 5;
+
+
+bool IsPalindrome()
+{
+    for (int i = 0; i < length / 2; i++)
+        while (number[i] == number[length - 1])
+        {
+            return true;
+        }
+    return false;
+}
+
+
+if (IsPalindrome())
+    
+        System.Console.WriteLine($"Число - {number} - является палиндромом");
+    
+else 
+    
+        System.Console.WriteLine($"Число - {number} НЕ является палиндромом");
---
+++ Home_Work_3/HW2_zadacha_21/Program.cs
@@ -0,0 +1,21 @@
+﻿//вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+int x1 = Cordinate("x", "A");
+int y1 = Cordinate("y", "A");
+int z1 = Cordinate("c", "A");
+int x2 = Cordinate("x", "B");
+int y2 = Cordinate("y", "B");
+int z2 = Cordinate("c", "B");
+
+int Cordinate(string coorName, string pointName)
+{
+    Console.WriteLine($"Введинте {coorName} точки {pointName}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+  
+double distance = Math.Round(Math.Sqrt(Math.Pow(x1-x2, 2) + Math.Pow(y1-y2, 2) + Math.Pow(z1-z2, 2)), 2);
+
+Console.WriteLine($"Расстояние между точками = {distance}");
+
+
+
